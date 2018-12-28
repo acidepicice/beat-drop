540f3dd833a7b50d703c318638ac538c7428f3fb
acidepicice <2021arao@tjhsst.edu>
2018-12-28 15:35:40 -0500

    Add vertical player movement

--- driver.py
+++ driver.py
@@ -13,7 +13,7 @@ canvas = Canvas(root, width=g.canvasWidth, height=g.canvasHeight, bg=g.backgroun
 canvas.pack()
 floorlist = []
 ball = None
-movingLeft, movingRight = False, False
+movingLeft, movingRight, movingUp, movingDown = False, False, False, False
 
 
 def setup() :
@@ -49,6 +49,16 @@ def moveRight(event) :
     movingLeft = False
     movingRight = True
 
+def moveUp(event) :
+    global movingUp, movingDown
+    movingUp = True
+    movingDown = False
+
+def moveDown(event) :
+    global movingUp, movingDown
+    movingUp = False
+    movingDown = True
+
 def stopLeft(event) :
     global movingLeft
     movingLeft = False
@@ -57,11 +67,22 @@ def stopRight(event) :
     global movingRight
     movingRight = False
 
+def stopUp(event) :
+    global movingUp
+    movingUp = False
+
+def stopDown(event) :
+    global movingDown
+    movingDown = False
 
 root.bind('<Left>', moveLeft)
 root.bind('<Right>', moveRight)
+root.bind('<Up>', moveUp)
+root.bind('<Down>', moveDown)
 root.bind('<KeyRelease-Left>', stopLeft)
 root.bind('<KeyRelease-Right>', stopRight)
+root.bind('<KeyRelease-Up>', stopUp)
+root.bind('<KeyRelease-Down>', stopDown)
 
 def tick() :
     global movingLeft, movingRight
@@ -77,12 +98,24 @@ def tick() :
             platform.x += g.platformSpeed
             platform.x2 += g.platformSpeed
     
+    if movingUp :
+        for platform in floorlist :
+            canvas.move(platform.body, 0, -g.platformSpeed)
+        Platform.y -= g.platformSpeed
+        Platform.y2 -= g.platformSpeed
+    
+    if movingDown :
+        for platform in floorlist :
+            canvas.move(platform.body, 0, g.platformSpeed)
+        Platform.y += g.platformSpeed
+        Platform.y2 += g.platformSpeed
+  
     ball.move(g.ballSpeed)
 
     if ball.x <= 0 or ball.x2 >= g.canvasWidth :
         g.ballSpeed *= -1
 
-    if ball.y2 > Platform.y and ball.y2 < Platform.y2 :
+    if (ball.y2 > Platform.y and ball.y2 < Platform.y2) or (ball.y > Platform.y and ball.y < Platform.y2) :
         for platform in floorlist :
             if (ball.x > platform.x and ball.x < platform.x2) or  (ball.x2 > platform.x and ball.x2 < platform.x2):
                 setup()
